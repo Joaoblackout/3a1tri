779a1968a0f76d82b0f6daa37a5865a1f8bc0a79
Joaoblackout <132076835+Joaoblackout@users.noreply.github.com>
2024-03-18 11:18:03 -0300

    dvasfd

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 <script src="main.js"></script>
 const botoes = document.querySelectorAll(".botao");
 console.log(botoes)
+for (let i=0; i < botões.length; i++) { 
+    console.log(i);
+ }
+ for(let i=0;i <botoes.length;i++){
+    botoes[i].onclick = function() {
+    botoes[i].classList.add(«ativo»);
+    }
+    }
